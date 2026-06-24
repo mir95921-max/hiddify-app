74123ecd3f652f2ce365be380181a5712a75a515
mir95921-max <mir95921@gmail.com>
2026-06-25 01:07:41 +0500

    brand: full VextraVPN rebrand (name, links, translations, logo, native window titles)

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -3,15 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:hiddify/utils/utils.dart';
 
 abstract class Constants {
-  static const appName = "Hiddify";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
-  static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
-  static const githubReleasesApiUrl = "https://api.github.com/repos/hiddify/hiddify-next/releases";
-  static const githubLatestReleaseUrl = "https://github.com/hiddify/hiddify-app/releases/latest";
+  static const appName = "VextraVPN";
+  static const githubUrl = "https://github.com/mir95921-max/hiddify-app";
+  static const licenseUrl = "https://github.com/mir95921-max/hiddify-app?tab=License-1-ov-file#readme";
+  static const githubReleasesApiUrl = "https://api.github.com/repos/mir95921-max/hiddify-app/releases";
+  static const githubLatestReleaseUrl = "https://github.com/mir95921-max/hiddify-app/releases/latest";
   static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
-  static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
-  static const termsAndConditionsUrl = "https://hiddify.com/terms/";
+  static const telegramChannelUrl = "https://t.me/Vextravpn_bot";
+  static const privacyPolicyUrl = "https://vextravpn.5-189-138-96.sslip.io/privacy-policy";
+  static const termsAndConditionsUrl = "https://vextravpn.5-189-138-96.sslip.io/terms";
   static const cfWarpPrivacyPolicy = "https://www.cloudflare.com/application/privacypolicy/";
   static const cfWarpTermsOfService = "https://www.cloudflare.com/application/terms/";
 }
